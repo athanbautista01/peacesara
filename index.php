c6d907f447f9debd1921b98b08c60f0927d083e8
Jonathan Bautista <jonathanbautista1997@gmail.com>
2016-09-24 15:53:07 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Peacesara</title>
+
+  <!-- Bootstrap -->
+  <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom css -->
+  <link href="assets/custom/css/style.css" rel="stylesheet">
+  <!-- Fonts -->
+  <link href="font-awesome-4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400;300' rel='stylesheet' type='text/css'>
+  <!-- FAVICON -->
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <!-- Custom js -->
+  <script src="assets/custom/js/custom.js"></script>
+</head>
+<body>
+
+  <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+  <div class="site-wrapper" id="main">
+
+    <div class="site-wrapper-inner">
+
+      <div class="container">
+        <div class="cover-container">
+        <div class="masthead clearfix">
+          <div class="inner">
+
+            <h3 class="masthead-brand"><img height="70px" src="img/peacesara.png"> PEACESARA</h3>
+            <nav>
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="#">HOME</a></li>
+                <li><a href="#">LOGIN</a></li>
+                <li onclick="openNav()"><a href="#">MENU</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        </div>
+
+        <div class="col-lg-6">
+        <hr>
+        <a class="btn btn-default" href="#" role="button">Start Discussion</a>
+        <br>
+        <hr>
+        <h2>Ongoing Discussions</h2>
+        <h3><a href="#">War and Peace: "Sometimes violence is needed to bring peace.” Do you agree with this?</a></h3>
+        <h3><a href="#">War and Peace: "Sometimes violence is needed to bring peace.” Do you agree with this?</a></h3>
+        <h3><a href="#">War and Peace: "Sometimes violence is needed to bring peace.” Do you agree with this?</a></h3>
+        <h3><a href="#">War and Peace: "Sometimes violence is needed to bring peace.” Do you agree with this?</a></h3>
+        <hr>
+        </div>
+        <hr>
+        <h3>Events</h2>
+        <hr>
+        <div class="col-lg-6">
+
+        </div>
+      </div>
+<div class="cover-container">
+      <div class="cover-container mastfoot">
+        <div class="inner">
+          <p>Copyright &copy; #PeaceHACK | <a href="#">Team Kinchana</a> 2016. All Rights Reserved</p>
+        </div>
+      </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Bootstrap js -->
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>

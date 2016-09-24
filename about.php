d88d6653ad3a6b143c82e1deec08b831af092487
Jonathan Bautista <jonathanbautista1997@gmail.com>
2016-09-25 02:56:37 +0800

    eight commit

--- about.php
+++ about.php
@@ -6,66 +6,68 @@ $title="Peacesara | About Us";
 include("include/header.php");
 ?>
 <hr>
-<img class="img-responsive" src="img/peacesara.png">
+<div class="col-lg-12">
+<center><img class="img-responsive" src="img/peacesara.png"></center>
 <h1 class="mb">About Peacesara</h1>
-<p>Peacesara is a website that contains topics that tackle different kinds of issues around the world. Peacesara aims to promote peace by allowing users to build friendship, trust, and share opinions with other users through debates, comments, and articles. And Uuers can also create new topics to discuss!</p>
 <br>
-
-
-<h2>About the Team</h2>
-<br>
-
-<div class="col-lg-4 col-md-4 col-sm-4">
-	<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14463748_1256385541049959_271235757_n.png?oh=25261cdbd5ce32bc034cb271e552587c&oe=57E8ADF5" width="200" height="200" alt="">
-	<h4>Jonathan Bautista</h4>
-	<h5 class="ctitle">CEO</h5>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<div class="hline"></div>
+<p class="thumbnail col-md-6 col-md-offset-3">Peacesara is a website that contains topics that tackle different kinds of issues around the world. Peacesara aims to promote peace by allowing users to build friendship, trust, and share opinions with other users through debates, comments, and articles. And Uuers can also create new topics to discuss!</p>
 </div>
 
+<div class="col-lg-12">
+	<h2>About the Team</h2>
+	<hr>
 
-<div class="col-lg-4 col-md-4 col-sm-4">
-	<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/13879378_1228176380534384_11714924711288670_n.jpg?oh=ab09b55e279b0f88571751b0fa0e5ce3&oe=587121B8" width="200" height="200" alt="">
-	<h4>Maria Ruffa Caranto</h4>
-	<h5 class="ctitle">CEO</h5>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<div class="hline"></div>
+	<div class="col-lg-4 col-md-4 col-sm-4">
+		<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14463748_1256385541049959_271235757_n.png?oh=25261cdbd5ce32bc034cb271e552587c&oe=57E8ADF5" width="200" height="200" alt="">
+		<h4>Jonathan Bautista</h4>
+		<h5 class="ctitle">CEO</h5>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="hline"></div>
+	</div>
+
+
+	<div class="col-lg-4 col-md-4 col-sm-4">
+		<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-9/13879378_1228176380534384_11714924711288670_n.jpg?oh=ab09b55e279b0f88571751b0fa0e5ce3&oe=587121B8" width="200" height="200" alt="">
+		<h4>Maria Ruffa Caranto</h4>
+		<h5 class="ctitle">CEO</h5>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="hline"></div>
+	</div>
+
+	<div class="col-lg-4 col-md-4 col-sm-4">
+		<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14463689_1256389277716252_1257819315_n.png?oh=b39d79e90525bb57e4b8b433a3b6f906&oe=57E8C98D" width="200" height="200" alt="">
+		<h4>Renche Adams Dalida</h4>
+		<h5 class="ctitle">CEO</h5>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="hline"></div>
+	</div>
+
+
+	<div class="col-lg-4 col-md-4 col-sm-4">
+		<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14445674_1256388031049710_644496694_n.png?oh=fff38a54bd7c4799ca07ae273d31ced9&oe=57E86FB2" width="200" height="200" alt="">
+		<h4>Madelle Dela Cruz</h4>
+		<h5 class="ctitle">CEO</h5>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="hline"></div>
+	</div>
+
+	<div class="col-lg-4 col-md-4 col-sm-4">
+		<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14458928_1256391894382657_7762578_n.png?oh=a8a7a1bc2c31c2f0fd3fa06196733b78&oe=57E88FB2" width="200" height="200" alt="">
+		<h4>Ivy Diane Dela Merced</h4>
+		<h5 class="ctitle">CEO</h5>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="hline"></div>
+	</div>
+
+
+	<div class="col-lg-4 col-md-4 col-sm-4">
+		<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14445630_1256392977715882_108658112_n.png?oh=a3968230a159e451ca851262f84bc717&oe=57E870FB" width="200" height="200" alt="">
+		<h4>Kristel Rica Uy</h4>
+		<h5 class="ctitle">CEO</h5>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+		<div class="hline"></div>
+	</div>
 </div>
-
-<div class="col-lg-4 col-md-4 col-sm-4">
-	<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14463689_1256389277716252_1257819315_n.png?oh=b39d79e90525bb57e4b8b433a3b6f906&oe=57E8C98D" width="200" height="200" alt="">
-	<h4>Renche Adams Dalida</h4>
-	<h5 class="ctitle">CEO</h5>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<div class="hline"></div>
-</div>
-
-
-<div class="col-lg-4 col-md-4 col-sm-4">
-	<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14445674_1256388031049710_644496694_n.png?oh=fff38a54bd7c4799ca07ae273d31ced9&oe=57E86FB2" width="200" height="200" alt="">
-	<h4>Madelle Dela Cruz</h4>
-	<h5 class="ctitle">CEO</h5>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<div class="hline"></div>
-</div>
-
-<div class="col-lg-4 col-md-4 col-sm-4">
-	<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14458928_1256391894382657_7762578_n.png?oh=a8a7a1bc2c31c2f0fd3fa06196733b78&oe=57E88FB2" width="200" height="200" alt="">
-	<h4>Ivy Diane Dela Merced</h4>
-	<h5 class="ctitle">CEO</h5>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<div class="hline"></div>
-</div>
-
-
-<div class="col-lg-4 col-md-4 col-sm-4">
-	<img src="https://scontent-sin6-1.xx.fbcdn.net/v/t34.0-12/14445630_1256392977715882_108658112_n.png?oh=a3968230a159e451ca851262f84bc717&oe=57E870FB" width="200" height="200" alt="">
-	<h4>Kristel Rica Uy</h4>
-	<h5 class="ctitle">CEO</h5>
-	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-	<div class="hline"></div>
-</div>
-
 <hr>
 <br>
 <?php

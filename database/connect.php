ab59e39f2e33348b087f6dc53d57969db54e1770
Jonathan Bautista <jonathanbautista1997@gmail.com>
2016-09-25 02:41:52 +0800

    updated database setting

--- database/connect.php
+++ database/connect.php
@@ -1,9 +1,14 @@
 <?php
     
     session_start();
-    $host = "localhost";
+    /*$host = "localhost";
     $user = "peacesara";
     $password = "peacesarapass";
+    $database = "peacesara";*/
+
+    $host = "ap-cdbr-azure-southeast-b.cloudapp.net";
+    $user = "bebc90aabc7f02";
+    $password = "3e901204";
     $database = "peacesara";
     //Connect to MySQL
     $conn = new mysqli($host, $user, $password, $database);
